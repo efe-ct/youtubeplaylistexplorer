a3ad95c54b9097181210a540b7f91ebad4c7d865
Nnanna Osinachukwu <114106754+efe-ct@users.noreply.github.com>
2025-07-08 12:45:11 +0000

    fixed tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-@type {import('tailwindcss').Config}
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
